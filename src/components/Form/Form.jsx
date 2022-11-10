43be2a77ce06277e87e99c899ce0c6e51a9412dd
Ruth Dantas <ruthdevgp@gmail.com>
2022-11-10 17:48:14 -0300

    organizando component form e fazendo requisicção post com axios

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,8 @@
 import { Box, Button, Container,  Typography } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import axios from 'axios';
+import { configure } from 'locales';
+import { useEffect, useState } from 'react';
 import { MyTextField } from './MyTextField';
 
 const theme = createTheme({
@@ -21,7 +24,7 @@ const theme = createTheme({
         root: {
           // Some CSS
           color: 'purple',
-          fontSize: '1.2rem',
+          fontSize: '1rem',
           fontWeight: 'bold',
         },
       },
@@ -30,22 +33,44 @@ const theme = createTheme({
 });
 
 export function Form({titleForm}) {
+
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
   
+
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setAuthor(event.target.value);
+  };
+
+ 
+  const postData = () => {
+    axios.post('https://vidaemcorrotina-api.herokuapp.com/posts', {
+      authorId: 'c98f9d9b-a96c-474c-a1d6-a242c6c9078d',
+      title,
+      text
+    }, { 'Content-Type': 'application/json'}).then(response => {
+      console.log(response.data);
+    }).catch(error => console.log(error));
+  };
+
   return (
     <Container sx={{position: 'relative', minHeight: '100vh'}}>
-      <Box component="form" noValidate autoComplete="off" sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translateY(-50%) translateX(-50%)'}}>
+      <Box component="form" noValidate autoComplete='off' sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translateY(-50%) translateX(-50%)'}}>
         <Box sx={{background: '#202024', padding: '8rem', borderRadius: '0.5rem', display: 'grid', gap: '2rem', width: '80rem'}}>
           <Typography sx={{color: 'var(--white)', fontSize: '2.5rem', display:' flex', flexDirection: 'column', alignItems: 'center'}}>{titleForm}</Typography>
           <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
             <ThemeProvider theme={theme}>
-              <MyTextField required id="outlined-required" label="Autor(a)" defaultValue="Seu nome" color="secondary" size='small' />
-              <MyTextField required id="outlined-required" label="Título" defaultValue="Título da Postagem" color="secondary" size='small' sx={{p: {color: '#fff'}}} />
+              <MyTextField required  id="outlined-text" label="Autor(a)" color="secondary" size='small' value={author} onChange={handleChange} />
+              <MyTextField required id="outlined-text" label="Título" color="secondary" size='small' value={title} onChange={(event) => {event.preventDefault(); setTitle(event.target.value);}}  />
             </ThemeProvider>
           </Box>
           <ThemeProvider theme={theme}>
-            <MyTextField required id="filled-multiline-flexible" label="Texto" defaultValue="Escreva seu texto" color="secondary" multiline  />
+            <MyTextField required id="filled-multiline-flexible" label="Texto" color="secondary" multiline value={text}  onChange={(event) => {event.preventDefault(); setText(event.target.value); }}/>
           </ThemeProvider>
-          <Button sx={{ color: '#1a1a1e', background: '#fff' }}>ENVIAR</Button>
+          <Button onClick={postData} sx={{ color: '#1a1a1e', background: '#fff' }}>ENVIAR</Button>
         </Box>
       </Box>
     </Container>
--- src/components/Form/MyTextField.jsx
+++ src/components/Form/MyTextField.jsx
@@ -1,7 +1,7 @@
 import { TextField } from '@mui/material';
 
-export function MyTextField({required, id, label, defaultValue, color, maxRows, multiline, inputProps}) {
+export function MyTextField({required, id, label, defaultValue, color, maxRows, multiline, inputProps,  value, onChange, type, size}) {
   return (
-    <TextField required={required} id={id} label={label} defaultValue={defaultValue} color={color} maxRows={maxRows} multiline={multiline} InputProps={inputProps}/>
+    <TextField required={required} id={id} label={label} defaultValue={defaultValue} color={color} maxRows={maxRows} multiline={multiline} InputProps={inputProps} value={value} onChange={onChange} type={type} size={size} />
   );
 }
